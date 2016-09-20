c052eba02c0d5d29cc79e8f09b0ab5bc610ec18a
Ross Tierney <rossgtierney@gmail.com>
2016-09-20 03:39:16 +0100

    improvements to footer and textbox layout
    Scaled images

--- LexusMortgages/Models/ApplyOnline.cs
+++ LexusMortgages/Models/ApplyOnline.cs
@@ -23,7 +23,7 @@ namespace LexusMortgages.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Required, Display(Name = "DOB")]
         public DateTime dob { get; set; }
-        [Display(Name = "Martial")]
+        [Required, Display(Name = "Martial")]
         public string martial { get; set; }
         [Required, Display(Name = "Occupation")]
         public string occupation { get; set; }
@@ -33,11 +33,11 @@ namespace LexusMortgages.Models
         public int netMonthlyIncome { get; set; }
         [Required, Display(Name = "Other Income")]
         public int otherIncome { get; set; }
-        [Display(Name = "Mortgage Type")]
+        [Required, Display(Name = "Mortgage Type")]
         public string mortgageType { get; set; }
-        [Display(Name = "Property Type")]
+        [Required, Display(Name = "Property Type")]
         public string propertyType { get; set; }
-        [Display(Name = "Mortgage Term")]
+        [Required, Display(Name = "Mortgage Term")]
         public string mortgageTerm { get; set; }
         [Required, Display(Name = "Purchase Price")]
         public int purchasePrice { get; set; }
