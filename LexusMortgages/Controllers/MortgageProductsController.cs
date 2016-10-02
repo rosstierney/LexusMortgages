d7161508a02c6e6bd11f78f498455c0cfb03fe00
Ross Tierney <rossgtierney@gmail.com>
2016-10-02 23:49:32 +0100

    updates and fixes

---
+++ LexusMortgages/Controllers/MortgageProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LexusMortgages.Controllers
+{
+    public class MortgageProductsController : Controller
+    {
+        // GET: MortgageProducts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
